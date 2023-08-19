97b90d39b6baa20ccd72370cc34021653efaaef9
mwangea <mwangeamusa@gmail.com>
2023-08-19 22:21:04 +0300

    modify

---
+++ Day12/if_statements.py
@@ -0,0 +1,10 @@
+# Simple Python code example with an if statement
+
+# Define a variable
+age = 18
+
+# Check if the age is greater than or equal to 18
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not yet an adult.")
