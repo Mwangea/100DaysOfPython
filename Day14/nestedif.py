7b40b44e38e901fd4bb965c8f6d748174e2d2ada
mwangea <mwangeamusa@gmail.com>
2023-08-22 21:26:15 +0300

    new

---
+++ Day14/nestedif.py
@@ -0,0 +1,16 @@
+# Nested if statement example
+
+# Define variables
+is_raining = True
+have_umbrella = False
+
+if is_raining:
+    print("It's raining!")
+
+    if have_umbrella:
+        print("You have an umbrella. Stay dry!")
+    else:
+        print("You don't have an umbrella. Stay indoors.")
+
+else:
+    print("It's not raining. Enjoy the weather!")
